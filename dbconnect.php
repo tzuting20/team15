927d21588bbc9839eea84fdab0fef13ccf08cd33
Jiale Chan嘉樂 <jialechan98@gmail.com>
2020-11-19 15:51:15 +0800

    stuList db NOT connected

--- dbconnect.php
+++ dbconnect.php
@@ -2,11 +2,7 @@
 $host = 'localhost';
 $user = 'root';
 $pass = '';
-<<<<<<< HEAD
 $db = '1091se';
-=======
-$db = '1901se';
->>>>>>> a7db5ad0f2a1a3b72ff107af2b9028748baa1812
 $conn = mysqli_connect($host, $user, $pass, $db) or die('Error with MySQL connection'); //跟MyMSQL連線並登入
 mysqli_query($conn,"SET NAMES utf8"); //選擇編碼
 ?>
