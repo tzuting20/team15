723693673ce45936bcfc2da890e892dc251feff5
p4ajack <36707jack@gmail.com>
2020-11-19 16:06:08 +0800

    Update FormAdd.php

--- FormAdd.php
+++ FormAdd.php
@@ -7,7 +7,7 @@ $mother=mysqli_real_escape_string($conn,$_POST['mother']);
 $status=mysqli_real_escape_string($conn,$_POST['status']);
 
 if ($title) { //if title is not empty
-	$sql = "insert into form (name, stuID, father,mother, status) values ('$name', '$father','$mother',0);";
+	$sql = "insert into form (name, stuID, father,mother, status) values ('$name','$stuID','$father','$mother',0);";
 	mysqli_query($conn, $sql) or die("Insert failed, SQL query error"); //執行SQL
 	echo "Data added";
 } else {
