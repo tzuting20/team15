4624ee18432cddd73f24a1720708165db8e22683
p4ajack <36707jack@gmail.com>
2020-11-19 15:04:38 +0800

    Update Form.php

--- Form.php
+++ Form.php
@@ -12,16 +12,16 @@ require("dbconnect.php");
 <h1>貧困學生補助經費申請表</h1>
 <form method="post" action="Form.php">
 
-      task title: <input name="title" type="text" id="title" /> <br>
-
-      task description: <input name="msg" type="text" id="msg" /> <br>
+申請人（學生）: <input name="name" type="text" id="name" /> <br>
+學號: <input name="stuID" type="text" id="stuID" /> <br>
+父: <input name="father" type="text" id="father" /> <br>
+母: <input name="mother" type="text" id="mother" /> <br>
 	  
-	  status: <select  name="status" type="select" id="urgent" /> 
-					<option value='低收入戶'>低收入戶</option>
-					<option value='中低收入戶'>中低收入戶</option>
-					<option value='家庭突發因素'>家庭突發因素</option>
-					</select>
-	  <br>
+status: <select  name="status" type="select" id="urgent" /> 
+		<option value='低收入戶'>低收入戶</option>
+		<option value='中低收入戶'>中低收入戶</option>
+		<option value='家庭突發因素'>家庭突發因素</option>
+		</select><br>
 
       <input type="submit" name="Submit" value="送出" />
 	</form>
